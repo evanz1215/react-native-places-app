27eb54c0e1baa0538544aa6b5c5a2220c145f322
Evan <evanz1215@gmail.com>
2024-05-18 02:51:42 +0800

    feat: Add selected location marker on Map screen

--- screens/Map.js
+++ screens/Map.js
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { useState } from "react";
 import MapView, { Marker } from "react-native-maps";
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -10,7 +12,33 @@ const Map = () => {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({
+      lat,
+      lng,
+    });
+  };
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
